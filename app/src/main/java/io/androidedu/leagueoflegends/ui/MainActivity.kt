04d2e0f067b4309490657a218c9a16b878a20aef
Gökhan ÖZTÜRK <GokhanOZTURK@AndroidEdu.IO>
2022-01-16 10:49:35 +0300

    MainActivity.kt moved to ui package

--- app/src/main/java/io/androidedu/leagueoflegends/ui/MainActivity.kt
+++ app/src/main/java/io/androidedu/leagueoflegends/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package io.androidedu.leagueoflegends
+package io.androidedu.leagueoflegends.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.androidedu.leagueoflegends.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
